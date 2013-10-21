4cd98803200b0d77eb47717c9157f95f49faf1a2
gunendu <gunendu@gunendu-Inspiron-N5010.(none)>
2013-10-22 02:14:37 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Guestdevise::Application.config.secret_key_base = '08fdb6e5d00a51c85d174bcbc4f329c55fe5dc4f9bb5b3447ffd0d02c8e90f1af6bfb16f1a1bf2c83bef0a739ef4fe0a857f86f844d10ec3e8916c2be823f1da'
